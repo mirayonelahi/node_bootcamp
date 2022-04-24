c7567a9fad420a438197421e2af5f7adbd7d48a6
Ayon <mirelahi33@gmail.com>
2022-04-23 20:45:32 -0400

    Fix issues

--- 03-task-manager/task_ayon/db/connect.js
+++ 03-task-manager/task_ayon/db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://ayon:ayon1533@basictest.xf3y9.mongodb.net/Test?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
   mongoose.connect(url);
 };
