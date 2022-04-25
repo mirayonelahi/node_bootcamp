b1f23d26cf3034a92472b317583d060aa3f3d054
Ayon <mirelahi33@gmail.com>
2022-04-25 18:49:25 -0400

    Writing the test first depending on requirement

--- ayon-practise/api-tdd/app.test.js
+++ ayon-practise/api-tdd/app.test.js
@@ -2,5 +2,17 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("my awesome tests", () => {
-  it("first test", () => {});
+  it("api/ping -> returns json with success: true", () => {
+    return request(app)
+      .get("/api/ping")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            success: expect.any(Boolean),
+          })
+        );
+      });
+  });
 });
