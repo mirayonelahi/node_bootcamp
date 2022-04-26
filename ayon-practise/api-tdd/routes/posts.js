a942298c1b11eedcef81dfc9934928c81784eb45
Ayon <mirelahi33@gmail.com>
2022-04-25 20:11:52 -0400

    Fetch post and get tag

--- ayon-practise/api-tdd/routes/posts.js
+++ ayon-practise/api-tdd/routes/posts.js
@@ -1,22 +1,39 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios").default;
 
-const successInfo = {
-  posts: [
-    {
-      author: "Jon Abbott",
-      authorId: 4,
-      id: 95,
-      likes: 985,
-      popularity: 0.42,
-      reads: 55875,
-      tags: ["politics", "tech", "health", "history"],
-    },
-  ],
-};
+// let successInfo = {
+//   posts: [
+//     {
+//       author: "Jon Abbott",
+//       authorId: 4,
+//       id: 95,
+//       likes: 985,
+//       popularity: 0.42,
+//       reads: 55875,
+//       tags: ["politics", "tech", "health", "history"],
+//     },
+//   ],
+// };
+async function getUser(tag) {
+  const url = `https://api.hatchways.io/assessment/blog/posts?tag=${tag}`;
+  try {
+    const response = await axios.get(url);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.status(200).send(successInfo);
+  //fetch data from api
+  const tag = req.query.tag;
+  const data = getUser(tag);
+
+  data.then((response) => {
+    res.status(200).send(response.data);
+  });
 });
 
 module.exports = router;
